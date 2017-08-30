06711c46e3463665fa9dff0adb090f9b2a5c4bb8
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 14:05:48 -0400

    added vhost iterator

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,12 @@
 # init.pp
+class apache_mysql_manager (
+  Hash $vhosts,
+  ){
+
+  $vhosts.each |$vhost, $attributes| {
+    apache::vhost {
+      port => attributes['port'],
+      docroot => attributes['docroot'],
+    }
+  }
+}
