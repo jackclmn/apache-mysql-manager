00b88f2c0fbe8fedd709ae61fcd028521eeb6f1f
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 16:41:20 -0400

    mysql stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # init.pp
 class apache_mysql_manager (
   Hash $vhosts,
+  Hash $dbs,
   ){
   class { 'apache':
     default_vhost => false,
@@ -11,6 +12,17 @@ class apache_mysql_manager (
     apache::vhost { $vhost:
       port    => $attributes['port'],
       docroot => $attributes['docroot'],
+    }
+  }
+
+  contain ::mysql::server
+
+  $dbs.each |$db, $attributes| {
+    mysql::db { $db:
+      user     => 'myuser',
+      password => 'password',
+      host     => 'localhost',
+      grant    => ['SELECT', 'UPDATE'],
     }
   }
 }
